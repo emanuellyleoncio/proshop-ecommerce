686617e29ac206fd833c316fccdfac5e6c67697e
Emanuelly Leoncio <emanuelly.leoncio.ext@infracommerce.com.br>
2025-01-29 14:54:08 -0300

    ci: adding configs to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,9 +34,21 @@ app.get("/api/config/paypal", (req: Request, res: Response) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve();
-console.log("__dirname", __dirname);
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 setupSwagger(app);
 
